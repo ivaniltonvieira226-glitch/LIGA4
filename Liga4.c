bc3200d2c17d272ced9d57001251cf5f27271fc1
Ivanilton <ivaniltonvieira226@gmail.com>
2026-01-15 12:42:41 -0300

    falta o verificar diagonal

--- Liga4.c
+++ Liga4.c
@@ -4,11 +4,39 @@
 
 
 
+int verificarVitoria(int tabuleiro[6][7], int id1, int id2){
+    // verificar horizontalmente
+    for (int i = 0; i < 6; i++){
+        for (int j = 0; j < 4; j++){
+            if (tabuleiro[i][j] == id1 && tabuleiro[i][j+1] == id1 && tabuleiro[i][j+2] == id1 && tabuleiro[i][j+3] == id1){
+                return id1;
+            }
+            if (tabuleiro[i][j] == id2 && tabuleiro[i][j+1] == id2 && tabuleiro[i][j+2] == id2 && tabuleiro[i][j+3] == id2){
+                return id2;
+            }
+        }
+
+        // verificar verticalmente
+        for (int j = 0; j < 7; j++){
+            if (tabuleiro[i][j] == id1 && tabuleiro[i+1][j] == id1 && tabuleiro[i+2][j] == id1 && tabuleiro[i+3][j] == id1){
+                return id1;
+            }
+            if (tabuleiro[i][j] == id2 && tabuleiro[i+1][j] == id2 && tabuleiro[i+2][j] == id2 && tabuleiro[i+3][j] == id2){
+                return id2;
+            }
+        }
+    }
+
+    // verificar diagonalmente (esquerda para direita, não sei como fazer isso direito)
+
+}
+
 
 typedef struct {
     int id;
     char user[100];
 } Ficha;
+
 
 int main() {
    int tabuleiro[6][7] = {0};
@@ -19,7 +47,7 @@ int main() {
    puts("O jogo consiste em alinhar 4 peças na horizontal, vertical ou diagonal para vencer.");
 
    while(1){
-   printf("Escolha o tema do jogo:\n1 - Individual(Player contra o computador)\n2 - Dois players\n3 - Simulado(computador contra computador)\n 4 - Sair\n");
+   printf("Escolha o tema do jogo:\n1 - Individual(Player contra o computador)\n2 - Dois players\n3 - Simulado(computador contra computador)\n4 - Sair\n");
    scanf("%d", &tema);
 
     Ficha jogador1, jogador2;
@@ -27,12 +55,28 @@ int main() {
     if (tema == 1){
         printf("Tema Individual selecionado.\n");
         // Lógica para o tema individual
-
+        printf("Iniciando o jogo Player vs Computador...\n");
+        printf("O jogador 1 será você e o jogador 2 será o computador.\n");
         printf("Digite seu nome: ");
         scanf("%s", jogador1.user);
         jogador1.id = 1;
-        jogador2.user[0] = "Kamputer";
+        jogador2.user[0] = "LUCAS";
         jogador2.id = 2;
+
+        // exibir tabuleiro
+          printf("============LIG4++============\n");
+        printf("\n");
+        printf("  1   2   3   4   5   6   7  \n");
+        printf("\n");
+        for(int i = 0; i < 6; i++){
+            printf("|");
+            for(int j = 0; j < 7;j++){
+                printf(" %d |",tabuleiro[i][j]);
+            }
+            printf("\n");
+        } 
+        
+        printf("-----------------------------\n");
 
 
 
@@ -43,7 +87,7 @@ int main() {
         printf("Tema Simulado selecionado.\n");
         // Lógica para o tema simulado
     } else if (tema == 4){
-        printf("Tema inválido. Encerrando o jogo.\n");
+        printf("Encerrando o jogo.\n");
         break;
     }
 
